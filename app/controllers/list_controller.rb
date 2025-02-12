946e25e13cc569c792940cee1d9827f504e9e159
JeredHuck <jeredhuck@gmail.com>
2025-02-12 15:30:23 -0600

    list controller

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+    def index 
+        @lists = List.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        @list = List.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    def create
+        @list = List.new(list_params)
+        if @list.save
+            render json: @list, status: :created
+        else 
+            render json: @list.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        list = List.find(params[:id])
+        list.update!(list_params)
+        redirect_to list
+    end
+
+    private 
+    def list_params
+        params.expect(list: [:id, :title, :content])
+    end
+end
