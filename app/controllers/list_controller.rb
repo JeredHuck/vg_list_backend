0c82e271e3aa78a5d7b3c9bd6fe6465baeae4e0e
JeredHuck <jeredhuck@gmail.com>
2025-05-13 10:23:04 -0500

    updated user and list controllers

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,41 +1,41 @@
 class ListsController < ApplicationController
-    def index 
-        @lists = List.all
-        render json: ListBLueprint.render(lists)
-    end
+  def index
+    @lists = List.all
+    render json: ListBlueprint.render(@lists)
+  end
 
-    def show
-        @list = List.find(params[:id])
-        render json: ListBLueprint.render(list)
-    end
+  def show
+    @list = List.find(params[:id])
+    render json: ListBlueprint.render(@list)
+  end
 
-    def create
-        @list = List.new(list_params)
-        if @list.save
-            render json: ListBLueprint.render(list), status: :created
-        else 
-            render json: { errors: list.errors.full_messages }: :unprocessable_entity
-        end
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      render json: ListBlueprint.render(@list), status: :created
+    else
+      render json: { errors: @list.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def update
-        list = List.find(params[:id])
-        if list.update!(list_params)
-            render ListBLueprint.render(list)
-            redirect_to list
-        else 
-            redner json: { errors: list.errors.full_messages }: :unprocessable_entity
-        end
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      render json: ListBlueprint.render(@list)
+    else
+      render json: { errors: @list.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        list = List.find(params[:id])
-        list.destroy
-        render json: @list, status: :ok
-    end
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    render json: { message: "List deleted successfully" }, status: :ok
+  end
 
-    private 
-    def list_params
-        params.expect(list: [:title, :content])
-    end
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :content)
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,41 +1,40 @@
 class UserController < ApplicationController
-    def index
-        @users = users.all
-        render json: UserBlueprint.render(users)
-    end
+  def index
+    @users = User.all
+    render json: UserBlueprint.render(@users)
+  end
 
-    def show
-        @user = User.find(params[:id])
-        render json: UserBlueprint.render(user)
-    end
+  def show
+    @user = User.find(params[:id])
+    render json: UserBlueprint.render(@user)
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: UserBlueprint.render(user), status: :created
-        else 
-            render json: { errors: user.errors.full_messages }: :unprocessable_entity
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: UserBlueprint.render(@user), status: :created
+    else 
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def update
-        user = User.find(params[:id])
-        if user.update!(user_params)
-            render UserBlueprint.render(user)
-            redirect_to user
-        else 
-            render json: { errors: user.errors.full_messages }: :unprocessable_entity
-        end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render json: UserBlueprint.render(@user)
+    else 
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy
-        user = User.find(params[:id])
-        user.destroy
-        render json: @user, status: :ok
-    end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: { message: "User deleted successfully" }, status: :ok
+  end
 
-    private 
-    def user_params
-        params.expect(user: [:username, :password])
-    end
+  private 
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
