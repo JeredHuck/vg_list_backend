ccc231a3d64e9767fe1002b690b4642b9d71c352
JeredHuck <jeredhuck@gmail.com>
2025-05-15 09:46:47 -0500

    routes syntax error correction

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:show, :update, :destroy]
 
   resources :users do
-    resources :lists, only: :[:index, :create]
+    resources :lists, only: [:index, :create]
   end
 end
